1e6f2520fb427dfb04142a1e752871b5c7567d4e
Mansi <mansi.chikarmane@gmail.com>
2021-03-15 12:23:35 -0700

    youtube header

--- youtubeClone/youtube-clone/src/Header.js
+++ youtubeClone/youtube-clone/src/Header.js
@@ -1,24 +1,28 @@
 import React from 'react'
+import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import VideoCallSharpIcon from '@material-ui/icons/VideoCallSharp';
 import AppsSharpIcon from '@material-ui/icons/AppsSharp';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-
+// import Avatar from '@material-ui/icons/Avatar';
 function Header() {
   return (
     <div className="header">
-      <h1>I am a header</h1>
+     
       <MenuIcon />
       <img className="header__logo" src="https://cdn.havecamerawilltravel.com/photographer/files/2020/01/youtube-logo-new.jpg" alt="" />
+      
 
       <input type="text"/>
       <SearchIcon />
+
+
       <VideoCallSharpIcon/>
       <AppsSharpIcon/>
       <NotificationsIcon />
-      <AccountCircleIcon/>
+      <AccountCircleIcon />
     </div>
   );
 }
