a5b4cc2aaaaeb610afa40e3af9a837c3dea0facf
Mansi <mansi.chikarmane@gmail.com>
2021-03-16 16:29:17 -0700

    add react router

--- youtubeClone/youtube-clone/src/App.js
+++ youtubeClone/youtube-clone/src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="app">
