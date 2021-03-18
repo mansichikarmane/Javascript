5b1f4792af7fc8639a7cb00b9d2f75e505022060
Mansi <mansi.chikarmane@gmail.com>
2021-03-17 23:04:08 -0700

    practice typescript

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,9 +1,2 @@
-var number = 1;
-var count = 2;
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log("Finally: " + i);
-}
-doSomething();
+var count = 5;
+count = 'a';
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,12 +1,3 @@
-var number = 1;
-let count = 2;
 
-function doSomething() {
-  for (let i=0; i<5; i++) {
-    console.log(i);
-  }
-
-  console.log("Finally: " + i);
-}
-
-doSomething();
+let count = 5;
+count = 'a';
