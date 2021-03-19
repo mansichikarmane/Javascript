022a49b32482d94047660387f6edf95c4ac5ff72
Mansi <mansi.chikarmane@gmail.com>
2021-03-18 22:04:23 -0700

    rendered image component on angular server

--- angular/hello-world/src/app/records.service.ts
+++ angular/hello-world/src/app/records.service.ts
@@ -4,6 +4,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RecordsService {
+  info1: string[] = ["Adam Taylor", "E234", "at@abc.net"]
+  info2: string[] = ["Shawn Michael", "E236", "sm@abc.net"]
+  info3: string[] = ["Mark Fisher", "E764", "mf@abc.net"]
 
+  getinfo1(): string[]{
+    return this.info1
+  }
+
+  getinfo2(): string[]{
+    return this.info2
+  }
+
+  getinfo3(): string[]{
+    return this.info3
+  }
   constructor() { }
 }
